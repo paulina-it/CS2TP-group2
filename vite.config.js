fa3aea0c75bbc1d04f92477356cc0829a836f4ca
Polina Bovykina <paulina.northy@gmail.com>
2023-11-28 14:15:19 +0000

    vite config fixed

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                'resources/assets/scss/app.css',
+                'resources/assets/js/app.js',
             ],
             refresh: true,
         }),
