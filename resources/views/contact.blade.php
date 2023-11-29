0ea60f29a3646551617bc08677c91299e0febcf1
Polina Bovykina <paulina.northy@gmail.com>
2023-11-29 13:48:52 +0000

    contact us page backend finished: model and controller created, validation added, success message display added

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -36,7 +36,7 @@
                     <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                     <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
                     <g id="SVGRepo_iconCarrier">
-                        <path  id="inst-svg" fill-rule="evenodd" clip-rule="evenodd"
+                        <path id="inst-svg" fill-rule="evenodd" clip-rule="evenodd"
                             d="M2 6C2 3.79086 3.79086 2 6 2H18C20.2091 2 22 3.79086 22 6V18C22 20.2091 20.2091 22 18 22H6C3.79086 22 2 20.2091 2 18V6ZM6 4C4.89543 4 4 4.89543 4 6V18C4 19.1046 4.89543 20 6 20H18C19.1046 20 20 19.1046 20 18V6C20 4.89543 19.1046 4 18 4H6ZM12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9ZM7 12C7 9.23858 9.23858 7 12 7C14.7614 7 17 9.23858 17 12C17 14.7614 14.7614 17 12 17C9.23858 17 7 14.7614 7 12ZM17.5 8C18.3284 8 19 7.32843 19 6.5C19 5.67157 18.3284 5 17.5 5C16.6716 5 16 5.67157 16 6.5C16 7.32843 16.6716 8 17.5 8Z"
                             fill="#000000"></path>
                     </g>
@@ -69,8 +69,13 @@
                 </svg>
             </a>
         </div>
-        <form action="mailto:21mafiozi21@gmail.com" method="POST" enctype="text/plain" class="shadow-md rounded">
+        <form action="{{ route('contact.submit') }}" method="POST" class="rounded">
+            @if (session('success') != null) 
+                <p class="success-message ">{{ session('success')}}</p> 
+            
+            @endif
             <h2>Get in Touch</h2>
+            @csrf
             {{-- @guest --}}
             <div class="names">
                 <label for="first-name">First Name</label>
@@ -90,12 +95,18 @@
                 <option value="other">Other</option>
             </select>
             <label for="message">Message</label>
-            <textarea id="message" rows="4" name="message" placeholder="Write your thoughts here..."></textarea>
-            <button type="submit" class="text-white py-2 px-4 rounded">Send</button>
+            <textarea id="message" rows="4" name="text" placeholder="Write your thoughts here..."></textarea>
+            <button id="contact-submit-btn" type="submit" class="text-white py-2 px-4 rounded">Send</button>
         </form>
     </div>
 
     @include('layouts.footer')
+
+    <script>
+        document.querySelector('#contact-submit-btn').addEventListener('click', () => {
+            if ()
+        })
+    </script>
 </body>
 
 </html>
