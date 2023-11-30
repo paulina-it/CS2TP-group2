76a3bd46fd8e69aab8aa12f308bb59c92ce56b0c
mubtasimb <mubtasim.bep@gmail.com>
2023-11-30 11:09:25 +0000

    made shopping-cart files

---
+++ resources/views/shopping-cart.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping Cart</title>
+    <link rel="stylesheet" href="C:\Users\bepar\OneDrive\Documents\GitHub\CS2TP-group2\resources\assets\sass\components\_shoppingCart.scss">
+
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Shopping Cart</h1>
+            <div class="shopping">
+                <img src="images/cart.svg">
+                <span class="quantity">0</span>
+            </div>
+        </header>
+        <div class="list"></div>
+    </div>
+    <div class="card">
+        <h1>Card</h1>
+        <ul class="listCard"></ul>
+        <div class="checkout">
+            <div class="total">0</div>
+            <div class="closeShopping">Close</div>
+        </div>
+    </div>
+
+    <script src="app.js"></script>
+</body>
+</html>
