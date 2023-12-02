fd922590543d6b2f3ef5bcc5a5b26eff3a3012d2
mubtasimb <mubtasim.bep@gmail.com>
2023-12-02 22:09:05 +0000

    progress on shopping cart

--- resources/assets/js/cart.js
+++ resources/assets/js/cart.js
@@ -7,4 +7,55 @@ let total = document.querySelector('.total');
 let quantity = document.querySelector('.quantity');
 
 
-openShopping.addEventListener()
+openShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+
+closeShopping.addEventListener('click', ()=>{
+    body.classList.add('active');
+})
+
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+let products = [
+    {
+        id: 1,
+        name: 'BOOK NAME 1',
+        image: '1.PNG',
+        price: '£12.90'
+    },
+
+    {
+        id: 2,
+        name: 'BOOK NAME 2',
+        image: '2.PNG',
+        price: '£12.90'
+    },
+
+    {
+        id: 3,
+        name: 'BOOK NAME 3',
+        image: '3.PNG',
+        price: '£12.90'
+    },
+];
+
+let listCards = [];
+
+function initApp() {
+    products.forEach((value, key)=>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src ="image/${value.image}"/>
+            <div class= "title">${value.name}</div>
+            <div class= "price">${value.price.toLocalString()}</div>
+             <button onClick= "addToCard(${key})">Add to card</button>
+        `;
+        list.appendChild(newDiv);
+    })
+}
+
+initApp();
