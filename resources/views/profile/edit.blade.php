109c87986c533f65372bc414ff26cd32e7ff0091
Polina Bovykina <paulina.northy@gmail.com>
2023-12-05 10:56:49 +0000

    logout moved to navbar

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,10 +1,6 @@
 @extends('layouts.app')
 
 @section('main')
-    <form method="POST" action="{{route('logout')}}">
-        @csrf
-        <input type="submit" value="logout"/>
-    </form>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Profile') }}
