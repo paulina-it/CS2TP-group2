43f4f225e94db6a9c8a919aa7c8668a68c72c43d
mubtasimb <mubtasim.bep@gmail.com>
2023-12-07 15:29:48 +0000

    -redesign about us page

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -8,20 +8,25 @@
     </head>
     <body>
       @include('layouts.navigation')
-        <header class="aboutUs">About us - flippinpages</header>
-        <p>Welcome to flippinpages, a site developed by a small team for our readers that have interests in books</p>
-        <h2 class="mission">Our Mission</h2>
-        <p>At flippinpages, our mission for the site is to create a platform where book enthusiasts would be able to connect others that have similiar interests as them. Here you would be able to find a sense of community with other book enthusiasts.<br> Along with the plethora of books to choose from, we also provide reviews and discussions on the various books you will find throughout the site. This allows you and others to be able to share and see others insights  of books, authors and the latest trends. We aim to have both classic and latest literature to cater to a diverse range of readers. </p>
-        <h2 class="team">Our Team</h2>
-        <p>Meet the small development team that have worked on the project for the creation of this platform</p>
-        <ul class="project-members">
-        <p>Project Leader: Polina Bovykina</p>
-        <p>Front-end Developers: Gurvir Brar, Akash Bhatti, Mubtasim Bepari, Joel Campbell</p>
-        <p>Back-end Developers: Oliver Burnett-Kiernan, Sayira Begum, Nihar Priyadarshi</p>
-        </ul>
+        <section>
+          <div class="about-us">
+            <h1>About Us</h1>
+            <div class="container">
+              <div class="content">
+                <h3>Welcome to Flippinpages</h3>
+                <p>At flippinpages, our mission for the site is to create a platform where book enthusiasts would be able to connect others that have similiar interests as them. Here you would be able to find a sense of community with other book enthusiasts.<br> 
+                Along with the plethora of books to choose from, we also provide reviews and discussions on the various books you will find throughout the site. 
+                This allows you and others to be able to share and see others insights  of books, authors and the latest trends. We aim to have both classic and latest literature to cater to a diverse range of readers. </p>
+              </div>
+              <div class="image-section">
+                <img src="/images/about-us1.jpg" alt="">
+              </div>
+            </div>
+          </div>
+        </section>
 
         <script src="app.js"></script>
-      @include('layouts.footer')
+      
     </body>
-    
+    @include('layouts.footer')
 </html>
