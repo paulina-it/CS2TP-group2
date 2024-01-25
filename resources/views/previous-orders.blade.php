a9af21df5753dd034dc6f313a12af0a103563e15
Sayira Begum <145380310+SayiraB2003@users.noreply.github.com>
2024-01-25 18:30:23 +0000

    Created previous orders page file (empty) and the route.

---
+++ resources/views/previous-orders.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('localVite')
+    <!-- Include Bootstrap CSS -->
+    <link href="path/to/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Include jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+    <!-- Include Bootstrap JS -->
+    <script src="path/to/bootstrap/js/bootstrap.bundle.min.js"></script>
+@endsection
+@section('main')
+    <div class="main">
+        <h1>Previous Orders</h1>
+    </div>
+@endsection
