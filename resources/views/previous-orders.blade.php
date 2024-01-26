3376d27f51e4a774e3f102d581eba9aeb06e78a7
Sayira Begum <145380310+SayiraB2003@users.noreply.github.com>
2024-01-26 15:42:31 +0000

    Added rough orders table (dummy books) and basic table css.

--- resources/views/previous-orders.blade.php
+++ resources/views/previous-orders.blade.php
@@ -13,7 +13,46 @@
     <div class="main">
         <h1 class="title">Previous Orders</h1>
         <div class="prev-orders-list">
-            <h3 class="m-9 text-center">No previous orders</h2>
+            <table>
+                <tr>
+                    <th>Product</th>
+                    <th>Quantity</th>
+                    <th>Total</th>
+                    <th>Date Ordered</th>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="prev-order">
+                            <img class="book-img" src="/images/Anna-Karenina.jpg">
+                            <div>
+                                <p>Anna Karenina</p>
+                                <small>Price: £12.90</small>
+                                <br>
+                            </div>
+                        </div>
+                    </td>
+                    <td><input type="number" value="1"></td>
+                    <td>£12.90</td>
+                    <td>2024-01-05</td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <div class="prev-order">
+                            <img class="book-img" src="/images/Eugene-Onegin.jpg">
+                            <div>
+                                <p>Eugene Onegin</p>
+                                <small>Price: £12.90</small>
+                                <br>
+                            </div>
+                        </div>
+                    </td>
+                    <td><input type="number" value="1"></td>
+                    <td>£12.90</td>
+                    <td>2023-11-17</td>
+                </tr>
+            </table>
         </div>
-    </div>
+
+        {{-- <h3 class="m-9 text-center">No previous orders</h2> --}}
 @endsection
