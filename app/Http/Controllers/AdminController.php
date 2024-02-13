3caace6661cd5a1cc5e3a9811127ffe6258ba650
burgerking59 <oliverbk4@gmail.com>
2024-02-13 15:10:21 +0000

    added backend for admin orders page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Book;
+use App\Models\User;
+use App\Models\Guest;
+use App\Models\Order;
 
 class AdminController extends Controller
 {
@@ -25,5 +28,48 @@ class AdminController extends Controller
             'search' => $search,
             'category' => null
         ]);
+    }
+
+    public function orders() {
+        if (request('idSearch')) {
+            $orders = Order::where('user_id', request('idSearch'))->orWhere('guest_id', request('idSearch'))->get();
+        } else {
+            $orders = Order::all();
+        } 
+        if (request('filter') && request('filter') != "none") {
+            $orders = $orders->filter(function($item)
+                {
+                    if($item['status'] == request('filter'))
+                    {
+                        return $item;
+                    }
+            });
+        }
+        $orders = $orders->values();
+        $users = array();
+        foreach ($orders as $order) {
+            if ($order['user_id']) {
+                array_push($users, User::where('id', $order['user_id'])->get());
+            } else if ($order['guest_id']) {
+                array_push($users, Guest::where('id', $order['guest_id'])->get());
+            } else {
+                array_push($users, [["id" => null,
+                "firstName" => null,
+                "lastName" => null,
+                "phone" => null,
+                "email" => null,]]);
+            }
+        }
+        return view('admin/admin-orders', [
+            'orders' => $orders,
+            'users' => $users,
+        ]);
+    }
+
+    public function process($id) {
+        $order = Order::findOrFail($id);
+        $order->status = request("status");
+        $order->save();
+        return redirect('admin/orders');
     }
 }
